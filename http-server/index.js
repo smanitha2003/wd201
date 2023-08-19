20f5d8fc496a4d14f2aed0ff74d29158d198b844
smanitha2003 <116002891+smanitha2003@users.noreply.github.com>
2023-08-19 18:19:50 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,12 +1,11 @@
-  const http = require("http");
+
+const http = require("http");
 const fs = require("fs");
-const args=process.argv.slice(2);
-const portArg=args.findIndex(arg=> arg==='--port');
-const port=portArg!==-1 ? parseInt(args[portArg+1],10):3000;
+const t = require("minimist");
+const ar=t(process.argv.slice(2))
 let homeContent = "";
 let projectContent = "";
 let registContent="";
-
 fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err;
@@ -30,7 +29,7 @@ fs.readFile("registration.html", (err, registration) => {
   http
   .createServer((request, response) => {
     let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
+    response.writeHead(200, { "Content-Type": "text/html" });
     switch (url) {
       case "/project":
         response.write(projectContent);
@@ -46,5 +45,5 @@ fs.readFile("registration.html", (err, registration) => {
         break;
     }
   })
-  .listen(port);
+  .listen(ar.port);
       
